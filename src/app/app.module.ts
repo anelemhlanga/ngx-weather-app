f91fa7e5f553f05a6ec7450ede8464caa6d59e60
Anele Mhlanga <anelenmhlanga@gmail.com>
2023-01-23 15:56:59 +0200

    Update app.module.ts
    add WeatherModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { APP_KEY, ApiKey, APP_URL, ApiUrl } from './app.config';
+import { WeatherModule } from './weather/weather.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, WeatherModule],
   providers: [
     {
       provide: APP_KEY,
