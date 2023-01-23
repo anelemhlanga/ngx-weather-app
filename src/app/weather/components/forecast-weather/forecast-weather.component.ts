966dc92e86f8079f3c7d82f1fd853dd3e28a7085
Anele Mhlanga <anelenmhlanga@gmail.com>
2023-01-23 16:01:56 +0200

    add observer on forecast.ts

--- src/app/weather/components/forecast-weather/forecast-weather.component.ts
+++ src/app/weather/components/forecast-weather/forecast-weather.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-forecast-weather',
   templateUrl: './forecast-weather.component.html',
-  styleUrls: ['./forecast-weather.component.css']
+  styleUrls: ['./forecast-weather.component.css'],
 })
 export class ForecastWeatherComponent {
-
+  constructor(private apiService: ApiService) {
+    apiService.getCurrentLocation().subscribe((response) => {
+      console.log('respponse', response);
+    });
+  }
 }
