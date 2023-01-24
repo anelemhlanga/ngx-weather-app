2d5299313bae7f84159336c2b6e2e41c7cdea430
Anele Mhlanga <anelenmhlanga@gmail.com>
2023-01-24 15:21:12 +0200

    Update current-weather.component.ts

--- src/app/weather/components/current-weather/current-weather.component.ts
+++ src/app/weather/components/current-weather/current-weather.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import * as moment from 'moment';
+import { ApiService } from '../../services/api.service';
+import { ICurrent } from '../../types/current.interface';
 
 @Component({
   selector: 'app-current-weather',
   templateUrl: './current-weather.component.html',
-  styleUrls: ['./current-weather.component.css']
+  styleUrls: ['./current-weather.component.css'],
 })
 export class CurrentWeatherComponent {
+  weather: any;
 
+  constructor(private apiService: ApiService) {
+    apiService.getCurrentWeather().subscribe({
+      next: (response: any) => {
+        this.weather = response;
+      },
+      error: (error) => {
+        console.error('error', error);
+      },
+      complete: () => {
+        console.log('complete');
+      },
+    });
+  }
+
+  formatDateTime(value: number) {
+    return moment.unix(value).format('DD-MM-YYYY, H:MM');
+  }
 }
