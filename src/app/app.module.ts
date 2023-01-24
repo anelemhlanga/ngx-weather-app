fe9b762da3161b34dbb35798f29b6ff4507a1735
Anele Mhlanga <anelenmhlanga@gmail.com>
2023-01-24 10:32:36 +0200

    Update app.module.ts
    in providers []

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { APP_KEY, ApiKey, APP_URL, ApiUrl } from './app.config';
+import {
+  APP_KEY,
+  ApiKey,
+  APP_URL,
+  ApiUrl,
+  APP_UNIT,
+  ApiUnit,
+} from './app.config';
 import { WeatherModule } from './weather/weather.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, WeatherModule],
+  imports: [BrowserModule, AppRoutingModule, WeatherModule, HttpClientModule],
   providers: [
     {
       provide: APP_KEY,
@@ -17,6 +25,10 @@ import { WeatherModule } from './weather/weather.module';
     {
       provide: APP_URL,
       useValue: ApiUrl,
+    },
+    {
+      provide: APP_UNIT,
+      useValue: ApiUnit,
     },
   ],
   bootstrap: [AppComponent],
